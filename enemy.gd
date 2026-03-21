751bee69c2aafa24e267c312c943499793736a73
damianckziu <Damian.Fital@gmail.com>
2026-03-21 20:34:30 +0100

    Krew

--- enemy.gd
+++ enemy.gd
@@ -7,6 +7,8 @@ var velocity = Vector2()
 var stun = false
 var hp = 3
 
+var blood_particles = preload("res://blood_particles.tscn")
+
 func _process(delta):
 	if Global.player != null and stun == false:
 		velocity = global_position.direction_to(Global.player.global_position)
@@ -14,12 +16,15 @@ func _process(delta):
 		velocity = lerp(velocity, Vector2(0, 0), 0.3)
 	
 	global_position += velocity * speed * delta
-	
+	 
 	if hp <= 0:
+		if Global.node_creation_parent != null:
+			var blood_particles_instance = Global.instance_node(blood_particles, global_position, Global.node_creation_parent)
+			blood_particles_instance.rotation = velocity.angle()
 		queue_free()
 
 func _on_hitbox_area_entered(area):
-	if area.is_in_group("Enemy_damager"):
+	if area.is_in_group("Enemy_damager") and stun == false:
 		modulate = Color.WHITE
 		velocity = -velocity * 6
 		hp -= 1
