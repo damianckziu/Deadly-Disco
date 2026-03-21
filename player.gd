c64a4a6f97ade9127062b236df1dbc889d520760
damianckziu <Damian.Fital@gmail.com>
2026-03-21 17:33:04 +0100

    Pierwszy Commit

---
+++ player.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+var speed = 150
+var velocity = Vector2()
+
+func _process(delta):
+	velocity.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	velocity.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	
+	velocity = velocity.normalized()
+	
+	global_position += speed * velocity * delta
+ 
